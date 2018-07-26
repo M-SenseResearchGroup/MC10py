92d878fbd862b690a8df089607dc4f3cf8858a42
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-07-26 11:18:37 -0400

    Created __init__.py Added line to import LoadMC10.py functions in __init__.py
    Created setup.py
    Added setup text
    
    Created LoadMC10.py to store methods for loading MC10 data

---
+++ MC10py/LoadMC10.py
@@ -0,0 +1,15 @@
+def load_mc10(study_dir):
+    """
+    Load raw MC10 data from a study directory containing folders for each subjected as downloaded from the
+    BioStamp RC web portal
+
+    Parameters
+    ---------
+    study_dir : str
+        Base study directory containing subject folders.
+
+    Returns
+    -------
+    """
+
+    pass
---
+++ MC10py/__init__.py
@@ -0,0 +1,1 @@
+from LoadMC10 import load_mc10 as LoadMC10
